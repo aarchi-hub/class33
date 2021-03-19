6f7f41a1a2612142a2aefd4e7bacee51bbf68e8f
aarchi-hub <74824636+aarchi-hub@users.noreply.github.com>
2021-03-19 19:14:24 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,23 @@
+class Ball {
+    cunstructor(x,y,width,height,angle){
+var options = {
+    density :1,
+    frictionAir:0.005};
+
+   this.body =  Bodies.rectangle(x,y, width,height,options);
+   this.width = width;
+   this.height = height;
+   World.add(world,this.body)
+    }
+
+   display(){
+       var angle = this.body.angle;
+       Push();
+       translate(this.body.postion.X, this.body.position.Y)
+       rotate(angle)
+       ellipse(0,0,this.width,this.height)
+       Pop();
+
+   }
+
+}
